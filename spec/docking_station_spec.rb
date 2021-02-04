d13d1dc351f643ee1236e044f6e387318d089434
charlierdm <charlie.davismiller@gmail.com>
2021-02-04 16:30:00 +0000

    second-commit-step-10

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,12 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to(:release_bike) }
+
+  it 'releases a bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+
+end
